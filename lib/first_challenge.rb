0935837b4bbcf0e307c96ad52c0bbeea8ce2aa51
github username <github email address>
2019-01-09 14:57:47 -0500

    Completed Code Along - Manipulating Hashes Lab

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +15,13 @@ def first_challenge
   }
 
   #your code here
+contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete("strawberry")
+    end
+end
 
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
